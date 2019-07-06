dbeda1967a873f312b795bbc5f51fcf483173c7f
Matthew Enterline <matthew.j.enterline@gmail.com>
2019-07-06 13:23:49 -0400

    First Commit

---
+++ React.NET/ClientApp/src/components/FetchDataComponents.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import { WeatherForecast } from './WeatherForecast';
+
+export function FetchData() {
+    return (
+        <div>
+            <div> <WeatherForecast/> </div>
+            <div>
+                <h1> New Component </h1>
+            </div>
+        </div>
+    );
+}
